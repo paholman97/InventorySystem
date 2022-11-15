b0abda1068f19c301179c654865ad9b0f4396e5c
peter <peter@LAPTOP-HG1MIU5E>
2022-11-15 21:07:22 +0000

    Add project files.

---
+++ InventorySystem/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using InventorySystem.Data;
+using InventorySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InventorySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private InventoryContext inventoryContext { get; set; }
+
+        public HomeController(InventoryContext inventoryContext)
+        {
+            this.inventoryContext = inventoryContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<InventoryItem> inventoryItems = inventoryContext.InventoryItems.ToList();
+
+            return View(inventoryItems);
+        }
+
+        public IActionResult ItemDetails(int inventoryItemId)
+        {
+            InventoryItem inventoryItem = inventoryContext.InventoryItems.FirstOrDefault(x => x.InventoryItemID == inventoryItemId);
+
+            return View(inventoryItem);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InventorySystem/Data/InventoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventorySystem.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+    }
+
+    public class InventoryItem
+    { 
+        public int InventoryItemID { get; set; }
+        public string? ItemName { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
